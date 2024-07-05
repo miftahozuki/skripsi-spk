567da7bea50824933cbf5539a9a3e083bf6f0845
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2024-07-05 17:29:10 +0700

    fix logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -26,13 +26,13 @@ class LoginController extends Controller
 
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
         Auth::logout();
      
-        request()->session()->invalidate();
-     
-        request()->session()->regenerateToken();
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
      
         return redirect('/');
     }
