6206bc5f278af05ae366558fed8996af4aacc6b1
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2024-07-07 14:03:46 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,7 +56,6 @@ class UserController extends Controller
 
             $img = 'user-'.$id. '.' .$request->img->extension();
             $file = Storage::disk('tmp')->put('img/' .$img, file_get_contents($request->file('img')));
-            dd(Storage::url('tmp', $file));
             // $file = Storage::putFileAs('public', $request->file('img'), 'img/' .$img);
             // $request->file('img')->move(public_path('assets/img'), $img);
             $user->img = 'assets/img/'.$img;
